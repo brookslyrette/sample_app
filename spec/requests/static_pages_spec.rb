6f039a90107c2f5692a5c9d3cc24d418989839f1
Brooks Lyrette <brooks@dotsub.com>
2013-11-19 21:48:12 -0500

    Updated tests to use configured string

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  defaultTitle = "Ruby on Rails Tutorial Sample App | "
 
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title(defaultTitle + "Home")
     end
   end
 
@@ -16,7 +17,7 @@ describe "Static pages" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title(defaultTitle + "Help")
     end
   end
 
@@ -25,7 +26,7 @@ describe "Static pages" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title(defaultTitle + "About Us")
     end
   end
 
