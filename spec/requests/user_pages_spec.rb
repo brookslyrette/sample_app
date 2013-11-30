029b1d0a828323112edff1236a88e1cbcb353fde
Brooks Lyrette <brooks@dotsub.com>
2013-11-30 08:03:37 -0500

    Basic edit protection for non-signed in users

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -62,7 +62,10 @@ describe "User pages" do
   end
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit edit_user_path(user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
 
     describe "edit page" do
       it { should have_content("Update your profile") }
