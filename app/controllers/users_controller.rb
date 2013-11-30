eec559fb358c03e18929878e818c7c1314e0eb2c
Brooks Lyrette <brooks@dotsub.com>
2013-11-30 08:10:03 -0500

    Filter for same user when editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def new
     @user = User.new
   end
-  def show
 
+  def show
     @user = User.find(params[:id])
   end
 
@@ -21,11 +22,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -43,6 +42,11 @@ class UsersController < ApplicationController
 
   def signed_in_user
     redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 
 end
